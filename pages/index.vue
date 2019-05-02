fa2570ccc4dc13ef77a089d52db48be588672738
Alkhwarizm <iqbal.alkhowarizmi@gmail.com>
2019-05-02 11:46:05 +0700

    Drawn svg view ports for maps and charts.

--- pages/index.vue
+++ pages/index.vue
@@ -15,11 +15,13 @@
         <div class="row">
           <div class="grid padded w-full h-full">
             <h3 class="title">PRIZE DISTRIBUTION</h3>
+            <div class="dist prize-dist w-full" ref="prize-dist"></div>
           </div>
         </div>
         <div class="row">
           <div class="grid padded w-full h-full">
             <h3 class="title">PLAYER DISTRIBUTION</h3>
+            <div class="dist player-dist w-full" ref="player-dist"></div>
           </div>
         </div>
       </div>
@@ -46,29 +48,57 @@ export default {
       const str = input.match(/[0-9]+/g)[0];
       return Number.parseInt(str);
     },
-    drawMap: function () {
-      const mapWidth = this.$refs.map.clientWidth;
-      const mapHeight = mapWidth/2;
+    drawViewPort: function (elementRef, heightFactor = 0.5) {
+      const width = this.$refs[elementRef].clientWidth;
+      const height = width * heightFactor;
       
-      this.mapSvg = this.$d3.select('.map')
+      this[`${elementRef}Svg`] = this.$d3.select(`.${elementRef}`)
         .append('svg')
-          .attr('class', 'map')
-          .attr('viewBox', `0 0 ${mapWidth} ${mapHeight}`)
+          .attr('viewBox', `0 0 ${width} ${height}`)
           .style('width', '100%')
           .style('height', 'auto');
 
-      this.mapView = this.mapSvg.append('rect')
+      this[`${elementRef}View`] = this[`${elementRef}Svg`].append('rect')
         .attr('class', 'view')
         .attr('x', 0.5)
         .attr('y', 0.5)
-        .attr('width', mapWidth - 1)
-        .attr('height', mapHeight - 1)
+        .attr('width', width - 1)
+        .attr('height', height - 1)
         .attr('fill', this.$tw.colors.secondary)
         .style('opacity', 0.1)
+    },
+    drawMap: function () {
+      this.drawViewPort('map');
+      // const mapWidth = this.$refs.map.clientWidth;
+      // const mapHeight = mapWidth/2;
+      
+      // this.mapSvg = this.$d3.select('.map')
+      //   .append('svg')
+      //     .attr('class', 'map')
+      //     .attr('viewBox', `0 0 ${mapWidth} ${mapHeight}`)
+      //     .style('width', '100%')
+      //     .style('height', 'auto');
+
+      // this.mapView = this.mapSvg.append('rect')
+      //   .attr('class', 'view')
+      //   .attr('x', 0.5)
+      //   .attr('y', 0.5)
+      //   .attr('width', mapWidth - 1)
+      //   .attr('height', mapHeight - 1)
+      //   .attr('fill', this.$tw.colors.secondary)
+      //   .style('opacity', 0.1)
+    },
+    drawPrizeDist: function () {
+      this.drawViewPort('prize-dist', 0.35);
+    },
+    drawPlayerDist: function () {
+      this.drawViewPort('player-dist', 0.35);
     }
   },
   mounted() {
     this.drawMap();
+    this.drawPrizeDist();
+    this.drawPlayerDist();
   }
 }
 </script>
@@ -92,7 +122,7 @@ export default {
 .title {
   font-family: Orbitron;
   letter-spacing: 2pt;
-  @apply text-white text-left font-semibold mt-px;
+  @apply text-white text-left font-semibold mt-px mb-1;
 }
 
 .grid {
